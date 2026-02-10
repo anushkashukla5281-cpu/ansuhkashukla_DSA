618a7e4fc63b7e0c72cb3a3cfdc8f9f59027c172
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2026-02-10 21:59:07 +0530

    Add files via upload

---
+++ day33_strong_number.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int main()
+{
+    int num, temp, digit, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    temp = num;
+
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        sum = sum + factorial(digit);
+        temp = temp / 10;
+    }
+
+    if(sum == num)
+        cout << num << " is a Strong Number";
+    else
+        cout << num << " is NOT a Strong Number";
+
+    return 0;
+}
