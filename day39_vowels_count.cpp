7d53755ca03e5a2e324336625437c460089aa0e7
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2026-02-17 23:29:05 +0530

    Add files via upload

---
+++ day39_vowels_count.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int count = 0;
+
+    cout << "Enter a string: ";
+    getline(cin, str);   // To take full sentence input
+
+    for(int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+
+        // Check for both uppercase and lowercase vowels
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+           ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            count++;
+        }
+    }
+
+    cout << "Number of vowels = " << count;
+
+    return 0;
+}
