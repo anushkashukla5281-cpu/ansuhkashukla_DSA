1fe41871c4bd0ac3ef848be15fe1e4144e92d092
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2026-08-13 22:45:08 +0530

    Add files via upload

---
+++ day04_binary_search.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0;
+    int high = n - 1;
+    int found = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            found = mid;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (found != -1)
+        cout << "Element found at index " << found;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
